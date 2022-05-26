88d1a95579088925567529c83ddf236394ad1d64
Kokimoko4a <106096547+Kokimoko4a@users.noreply.github.com>
2022-05-26 17:32:50 +0300

    Added exercises from Data Types and Variables.

---
+++ Data Types and Variables/Concat Names/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Concat_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            string symbol = Console.ReadLine();
+            Console.WriteLine($"{name}{symbol}{lastName}");
+        }
+    }
+}
---
+++ Data Types and Variables/Special Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Special_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                int digit = i;
+                int sum = 0;
+
+                while (digit !=0)
+                {
+                    sum += digit % 10;
+                    digit /= 10;
+                }
+
+                if (sum == 5 || sum == 7 || sum == 11)
+                {
+                    Console.WriteLine($"{i} -> True");
+                }
+
+                else
+                {
+                    Console.WriteLine($"{i} -> False");
+                }
+            }
+        }
+    }
+}
