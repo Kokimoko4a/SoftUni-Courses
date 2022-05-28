9febb7f0610cd79af80f8f411dd6376dc20e5ec5
Kokimoko4a <106096547+Kokimoko4a@users.noreply.github.com>
2022-05-28 20:51:51 +0300

    Added all exercises from "Exercise Data Types and Variables".

---
+++ Fundamentals - May 2022/Data Types and Variables - Exercise/11.Snowballs/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Numerics;
+
+namespace _11.Snowballs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            BigInteger  numberOfSnowballs = BigInteger.Parse(Console.ReadLine());
+            BigInteger  theBest = int.MinValue;
+            BigInteger theSnow = 0;
+            BigInteger theTime = 0;
+            BigInteger theQuality = 0;
+
+            for (int i = 0; i < numberOfSnowballs ; i++)
+            {
+                BigInteger snowballSnow = BigInteger.Parse(Console.ReadLine());
+                BigInteger snowballTime = BigInteger.Parse(Console.ReadLine());
+                int snowballQuality = int.Parse(Console.ReadLine());
+
+                BigInteger  snowballValue = snowballSnow / snowballTime;
+                snowballValue = BigInteger.Pow(snowballValue , snowballQuality ); 
+
+                if (snowballValue > theBest )
+                {
+                    theBest = snowballValue;
+                    theSnow = snowballSnow;
+                    theTime = snowballTime;
+                    theQuality = snowballQuality;
+                }
+            }
+
+            Console.WriteLine($"{theSnow} : {theTime} = {theBest} ({theQuality})");
+
+        }
+    }
+}
