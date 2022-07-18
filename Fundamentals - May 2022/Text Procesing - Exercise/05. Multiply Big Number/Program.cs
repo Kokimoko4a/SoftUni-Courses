f5f74b49e69635f08e326cee0d6ed163d7f19e2a
Kokimoko4a <106096547+Kokimoko4a@users.noreply.github.com>
2022-07-18 11:44:03 +0300

    Added all exercises from Text Processing - Exercise.

---
+++ Fundamentals - May 2022/Text Procesing - Exercise/05. Multiply Big Number/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+
+namespace _05._Multiply_Big_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string bigNumber = Console.ReadLine();
+            int smallNumber = int.Parse(Console.ReadLine());
+
+            if (smallNumber == 0)
+            {
+                Console.WriteLine("0");
+                return;
+            }
+
+            int remainder = 0;
+            StringBuilder sumReversed = new StringBuilder();
+            StringBuilder sumNormal = new StringBuilder();
+
+            for (int i = bigNumber.Length - 1; i >= 0; i--)
+            {
+                int currSum = 0;
+                string currDigit = bigNumber[i].ToString();
+                int realyDigit = int.Parse(currDigit);
+                currSum = realyDigit * smallNumber + remainder;
+                sumReversed.Append(currSum % 10);
+                remainder = currSum / 10;
+
+            }
+
+            if (remainder != 0)
+            {
+                sumReversed.Append(remainder);
+            }
+
+            for (int i = sumReversed.Length - 1; i >= 0; i--)
+            {
+                sumNormal.Append(sumReversed[i]);
+            }
+
+            Console.WriteLine(sumNormal);
+        }
+    }
+}
