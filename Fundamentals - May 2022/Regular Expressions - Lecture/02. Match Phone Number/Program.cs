867e9735562f65141b5b0949ed771dc09dcd8cfa
Kokimoko4a <106096547+Kokimoko4a@users.noreply.github.com>
2022-07-21 16:03:00 +0300

    Added tasks from Regular Expressions - Lecture.

---
+++ Fundamentals - May 2022/Regular Expressions - Lecture/02. Match Phone Number/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02._Match_Phone_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"\+359(?<sep>[ -])2\k<sep>\d{3}\k<sep>\d{4}\b";
+            Regex regex = new Regex(pattern);
+            MatchCollection matchCollection = regex.Matches(input);
+            Console.WriteLine(string.Join(", ", matchCollection));
+        }
+    }
+}
