2c6aa13816f9e0308eb714d74391d7d9660e5a36
Kokimoko4a <106096547+Kokimoko4a@users.noreply.github.com>
2022-09-21 16:21:06 +0300

    Added more exercises from Multidimensional Arrays - Exercise.

---
+++ Advanced - September 2022/Multidimensional Arrays - Exercise/6. Jagged Array Manipulator/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Linq;
+
+namespace _6._Jagged_Array_Manipulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int[][] matrix = new int[rows][];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] currNumbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                matrix[row] = new int[currNumbers.Length];
+                for (int col = 0; col < currNumbers.Length; col++)
+                {
+                   
+                    matrix[row][col] = currNumbers[col];
+                }
+            }
+
+            for (int row = 0; row < rows-1; row++)
+            {
+                if (matrix[row].Length == matrix[row+1].Length)
+                {
+                    for (int i = 0; i < matrix[row].Length; i++)
+                    {
+                        matrix[row][i] *= 2;
+                        matrix[row + 1][i] *= 2;
+                    }
+                }
+
+                else
+                {
+                    for (int col = 0; col < matrix[row].Length; col++)
+                    {
+                        matrix[row][col] /= 2;
+                   
+                    }
+
+                    for (int col = 0; col < matrix[row+1].Length; col++)
+                    {
+                        matrix[row+1][col] /= 2;
+                    }
+                }
+            }
+
+            string command = Console.ReadLine();
+
+            while (command!= "End")
+            {
+                string[] tokens = command.Split();
+                string action = tokens[0];
+
+                if (action == "Add")
+                {
+                    int row = int.Parse(tokens[1]);
+                    int col = int.Parse(tokens[2]);
+                    int value = int.Parse(tokens[3]);
+
+                    if (row >= 0 && row < rows && col >= 0 && col < matrix[row].Length)
+                    {
+                        matrix[row][col] += value;
+                    }
+                }
+
+                else if (action == "Subtract")
+                {
+                    int row = int.Parse(tokens[1]);
+                    int col = int.Parse(tokens[2]);
+                    int value = int.Parse(tokens[3]);
+
+                    if (row >= 0 && row < rows && col >= 0 && col < matrix[row].Length)
+                    {
+                        matrix[row][col] -= value;
+                    }
+                }
+
+                command = Console.ReadLine();
+
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    Console.Write(matrix[row][col] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
