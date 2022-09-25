cffba6e9df01b44a099b7ab07807a3872d68f198
Kokimoko4a <106096547+Kokimoko4a@users.noreply.github.com>
2022-09-25 13:20:14 +0300

    Added exercises from ExamPrep.

---
+++ Advanced - September 2022/ExamPreparation/01. Barista Contest/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _01._Barista_Contest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> cofee = new Queue<int>(Console.ReadLine().Split(", ").Select(int.Parse));
+            Stack<int> milk = new Stack<int>(Console.ReadLine().Split(", ").Select(int.Parse));
+            Dictionary<int, Cofee> menu = new Dictionary<int, Cofee>();
+
+            menu.Add(50, new Cofee("Cortado" ,0));
+            menu.Add(75, new Cofee("Espresso", 0));
+            menu.Add(100, new Cofee("Capuccino", 0));
+            menu.Add(150, new Cofee("Americano", 0));
+            menu.Add(200, new Cofee("Latte", 0));
+
+            while (cofee.Count > 0 && milk.Count > 0)
+            {
+                int currCoffe = cofee.Dequeue();
+                int currMilk = milk.Pop();
+                int caffe = currMilk + currCoffe;
+
+                if (menu.ContainsKey(caffe))
+                {
+                    menu[caffe].Count++;
+                }
+
+                else
+                {
+                    currMilk -= 5;
+                    milk.Push(currMilk);
+                }
+            }
+
+            if (milk.Count == 0 && cofee.Count == 0)
+            {
+                Console.WriteLine("Nina is going to win! She used all the coffee and milk!");
+                Console.WriteLine("Coffee left: none");
+                // Console.Write(String.Join(", ",cofee));
+                Console.WriteLine("Milk left: none");
+
+                foreach (var item in menu.OrderBy(x => x.Value.Count).ThenByDescending(x => x.Value.Name))
+                {
+                    if (item.Value.Count>0)
+                    {
+                        Console.WriteLine($"{item.Value.Name}: {item.Value.Count}");
+                    }
+
+                    
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("Nina needs to exercise more! She didn't use all the coffee and milk!");
+
+                if (cofee.Count > 0)
+                {
+                    Console.Write("Coffee left: ");
+                    Console.WriteLine(String.Join(", ", cofee));
+                  
+                }
+
+                else
+                {
+                    Console.WriteLine("Coffee left: none");
+                }
+
+                if (milk.Count > 0)
+                {
+                    Console.Write("Milk left: ");
+                    Console.WriteLine(String.Join(", ", milk));
+                  
+                }
+
+                else
+                {
+                    Console.WriteLine("Milk left: none");
+                }
+
+                foreach (var item in menu.OrderBy(x => x.Value.Count).ThenByDescending(x => x.Value.Name))
+                {
+                    if (item.Value.Count > 0)
+                    {
+                        Console.WriteLine($"{item.Value.Name}: {item.Value.Count}");
+                    }
+                }
+            }
+        }
+        class Cofee
+        {
+            public Cofee(string name, int count)
+            {
+                Name = name;
+                Count = count;
+            }
+
+            public string Name { get; set; }
+            public int Count { get; set; }
+
+        }
+    }
+}
+
+
+
---
+++ Advanced - September 2022/ExamPreparation/02. Help-A-Mole/Program.cs
@@ -0,0 +1,289 @@
+﻿using System;
+
+namespace _02._Help_A_Mole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[size, size];
+            int points = 0;
+            int rowOfMole = 0;
+            int colOfMole = 0;
+
+            for (int row = 0; row < size; row++)
+            {
+                string currElements = Console.ReadLine();
+
+                for (int col = 0; col < size; col++)
+                {
+                    matrix[row, col] = currElements[col];
+
+                    if (matrix[row, col] == 'M')
+                    {
+                        rowOfMole = row;
+                        colOfMole = col;
+
+                    }
+                }
+            }
+
+            string command = Console.ReadLine();
+
+            while (points < 25)
+            {
+                if (command == "End")
+                {
+                    break;
+                }
+
+                if (command == "left")
+                {
+                    if (IsCellValid(matrix, rowOfMole, colOfMole - 1, size))
+                    {
+                        char currElement = matrix[rowOfMole, colOfMole - 1];
+
+                        if (char.IsDigit(currElement))
+                        {
+                            points += int.Parse(currElement.ToString());
+                            matrix[rowOfMole, colOfMole - 1] = 'M';
+                            matrix[rowOfMole, colOfMole] = '-';
+                            colOfMole--;
+
+
+                            command = Console.ReadLine();
+                            continue;
+                        }
+
+                        if (currElement == 'S')
+                        {
+                            matrix[rowOfMole, colOfMole - 1] = '-';
+                            matrix[rowOfMole, colOfMole] = '-';
+                            points -= 3;
+
+                            for (int row = 0; row < size; row++)
+                            {
+                                for (int col = 0; col < size; col++)
+                                {
+                                    if (matrix[row, col] == 'S')
+                                    {
+                                        rowOfMole = row;
+                                        colOfMole = col;
+
+                                        matrix[rowOfMole, colOfMole] = 'M';
+                                    }
+                                }
+                            }
+
+                        }
+
+                        else
+                        {
+                            matrix[rowOfMole, colOfMole] = '-';
+                            matrix[rowOfMole, colOfMole - 1] = 'M';
+                            colOfMole--;
+                        }
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Don't try to escape the playing field!");
+                    }
+                }
+
+
+                else if (command == "right")
+                {
+                    if (IsCellValid(matrix, rowOfMole, colOfMole + 1, size))
+                    {
+                        char currElement = matrix[rowOfMole, colOfMole + 1];
+
+                        if (char.IsDigit(currElement))
+                        {
+                            points += int.Parse(currElement.ToString());
+                            matrix[rowOfMole, colOfMole + 1] = 'M';
+                            matrix[rowOfMole, colOfMole] = '-';
+                            colOfMole++;
+                            command = Console.ReadLine();
+                            continue;
+
+                        }
+
+                        if (currElement == 'S')
+                        {
+                            matrix[rowOfMole, colOfMole + 1] = '-';
+                            matrix[rowOfMole, colOfMole] = '-';
+                            points -= 3;
+
+                            for (int row = 0; row < size; row++)
+                            {
+                                for (int col = 0; col < size; col++)
+                                {
+                                    if (matrix[row, col] == 'S')
+                                    {
+                                        rowOfMole = row;
+                                        colOfMole = col;
+
+                                        matrix[rowOfMole, colOfMole] = 'M';
+                                    }
+                                }
+                            }
+
+                        }
+
+                        else
+                        {
+                            matrix[rowOfMole, colOfMole] = '-';
+                            matrix[rowOfMole, colOfMole + 1] = 'M';
+                            colOfMole++;
+                        }
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Don't try to escape the playing field!");
+                    }
+                }
+
+                else if (command == "up")
+                {
+                    if (IsCellValid(matrix, rowOfMole - 1, colOfMole, size))
+                    {
+                        char currElement = matrix[rowOfMole - 1, colOfMole];
+
+                        if (char.IsDigit(currElement))
+                        {
+                            points += int.Parse(currElement.ToString());
+                            matrix[rowOfMole - 1, colOfMole] = 'M';
+                            matrix[rowOfMole, colOfMole] = '-';
+                            rowOfMole--;
+                            command = Console.ReadLine();
+                            continue;
+
+                        }
+
+                        if (currElement == 'S')
+                        {
+                            matrix[rowOfMole - 1, colOfMole] = '-';
+                            matrix[rowOfMole, colOfMole] = '-';
+                            points -= 3;
+
+                            for (int row = 0; row < size; row++)
+                            {
+                                for (int col = 0; col < size; col++)
+                                {
+                                    if (matrix[row, col] == 'S')
+                                    {
+                                        rowOfMole = row;
+                                        colOfMole = col;
+
+                                        matrix[rowOfMole, colOfMole] = 'M';
+                                    }
+                                }
+                            }
+
+                        }
+
+                        else
+                        {
+                            matrix[rowOfMole, colOfMole] = '-';
+                            matrix[rowOfMole - 1, colOfMole] = 'M';
+                            rowOfMole--;
+                        }
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Don't try to escape the playing field!");
+                    }
+                }
+
+                else if (command == "down")
+                {
+                    if (IsCellValid(matrix, rowOfMole + 1, colOfMole, size))
+                    {
+                        char currElement = matrix[rowOfMole + 1, colOfMole];
+
+                        if (char.IsDigit(currElement))
+                        {
+                            points += int.Parse(currElement.ToString());
+                            matrix[rowOfMole + 1, colOfMole] = 'M';
+                            matrix[rowOfMole, colOfMole] = '-';
+                            rowOfMole++;
+                            command = Console.ReadLine();
+                            continue;
+
+                        }
+
+                        if (currElement == 'S')
+                        {
+                            matrix[rowOfMole + 1, colOfMole] = '-';
+                            matrix[rowOfMole, colOfMole] = '-';
+                            points -= 3;
+
+                            for (int row = 0; row < size; row++)
+                            {
+                                for (int col = 0; col < size; col++)
+                                {
+                                    if (matrix[row, col] == 'S')
+                                    {
+                                        rowOfMole = row;
+                                        colOfMole = col;
+
+                                        matrix[rowOfMole, colOfMole] = 'M';
+                                    }
+                                }
+                            }
+
+                        }
+
+                        else
+                        {
+                            matrix[rowOfMole, colOfMole] = '-';
+                            matrix[rowOfMole + 1, colOfMole] = 'M';
+                            rowOfMole++;
+                        }
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Don't try to escape the playing field!");
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            if (points >= 25)
+            {
+                Console.WriteLine("Yay! The Mole survived another game!");
+                Console.WriteLine($"The Mole managed to survive with a total of {points} points.");
+            }
+
+            else
+            {
+                Console.WriteLine("Too bad! The Mole lost this battle!");
+                Console.WriteLine($"The Mole lost the game with a total of {points} points.");
+            }
+
+            for (int row = 0; row < size; row++)
+            {
+
+
+                for (int col = 0; col < size; col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static bool IsCellValid(char[,] matrix, int rowOfmole, int colOfmole, int size)
+        {
+            return rowOfmole >= 0 && rowOfmole < size && colOfmole >= 0 && colOfmole < size;
+        }
+    }
+}
+
