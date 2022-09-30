dba23620c29bbf5665796d982e884bc0c879e4bc
Kokimoko4a <106096547+Kokimoko4a@users.noreply.github.com>
2022-09-30 17:01:08 +0300

    Added exercises from Functional Programming - Lecture.

---
+++ Advanced - September 2022/Functional Programming - Lecture/01. Sort Even Numbers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Sort_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).Where(x => x % 2 == 0).OrderBy(x => x).ToArray();
+            Console.WriteLine(String.Join(", ", numbers));
+        }
+    }
+}
