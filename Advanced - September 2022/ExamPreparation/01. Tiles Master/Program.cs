04a346eab3c05fe65adcab1272a0ec012d9b75fe
Kokimoko4a <106096547+Kokimoko4a@users.noreply.github.com>
2022-09-30 17:02:59 +0300

    Added more exercises from ExamPreparation.

---
+++ Advanced - September 2022/ExamPreparation/01. Tiles Master/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Tiles_Master
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> whiteTiles = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+            Queue<int> greyTiles = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+            Dictionary<string, Location> locationsAndTiles = new Dictionary<string, Location>();
+            locationsAndTiles.Add("40", new Location("Sink", 0));
+            locationsAndTiles.Add("50", new Location("Oven", 0));
+            locationsAndTiles.Add("60", new Location("Countertop", 0));
+            locationsAndTiles.Add("70", new Location("Wall", 0));
+            locationsAndTiles.Add("Floor", new Location("Floor", 0));
+
+
+            while (whiteTiles.Count > 0 && greyTiles.Count > 0)
+            {
+                int currWhiteTile = whiteTiles.Pop();
+                int currGreyTile = greyTiles.Dequeue();
+                int newTile = currWhiteTile + currGreyTile;
+
+                if (currWhiteTile == currGreyTile)
+                {
+                    if (locationsAndTiles.ContainsKey(newTile.ToString()))
+                    {
+                        locationsAndTiles[newTile.ToString()].Count++;
+                    }
+
+                    else
+                    {
+                        locationsAndTiles["Floor"].Count++;
+                    }
+                }
+
+                else
+                {
+                    currWhiteTile /= 2;
+                    whiteTiles.Push(currWhiteTile);
+                    greyTiles.Enqueue(currGreyTile);
+                }
+            }
+
+            if (whiteTiles.Count>1)
+            {
+                Console.WriteLine($"White tiles left: {string.Join(", ",whiteTiles)}");
+            }
+
+            else
+            {
+                Console.WriteLine("White tiles left: none");
+            }
+
+            if (greyTiles.Count > 1)
+            {
+                Console.WriteLine($"Grey tiles left: {string.Join(", ", greyTiles)}");
+            }
+
+            else
+            {
+                Console.WriteLine("Grey tiles left: none");
+            }
+
+            foreach (var item in locationsAndTiles.OrderByDescending(x=>x.Value.Count).ThenBy(x=>x.Value.Name).Where(x=>x.Value.Count>0))
+            {
+                Console.WriteLine($"{item.Value.Name}: {item.Value.Count}");
+            }
+        }
+    }
+
+    class Location
+    {
+        public Location(string name, int count)
+        {
+            Name = name;
+            Count = count;
+        }
+        public string Name { get; set; }
+        public int Count { get; set; }
+
+    }
+}
---
+++ Advanced - September 2022/ExamPreparation/02. Wall Destroyer/Program.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Drawing;
+
+namespace _02._Wall_Destroyer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[size, size];
+            int countOfHoles = 0;
+            int vankoRow = -1;
+            int vankoCol = -1;
+            int hitrods = 0;
+
+            for (int row = 0; row < size; row++)
+            {
+                string currElements = Console.ReadLine();
+
+                for (int col = 0; col < size; col++)
+                {
+                    matrix[row, col] = currElements[col];
+
+                    if (matrix[row, col] == 'V')
+                    {
+                        vankoRow = row;
+                        vankoCol = col;
+
+                    }
+                }
+            }
+
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                if (command == "left")
+                {
+                    if (CellValid(vankoCol - 1, vankoRow, size))
+                    {
+                        char futureElement = matrix[vankoRow, vankoCol - 1];
+
+                        if (futureElement == '-')
+                        {
+                            matrix[vankoRow, vankoCol] = '*';
+                            matrix[vankoRow, vankoCol - 1] = 'V';
+                            vankoCol--;
+                            countOfHoles++;
+                        }
+
+                        else if (futureElement == 'C')
+                        {
+                            matrix[vankoRow, vankoCol] = '*';
+                            matrix[vankoRow, vankoCol - 1] = 'E';
+                            countOfHoles++;
+                            Console.WriteLine($"Vanko got electrocuted, but he managed to make {++countOfHoles} hole(s).");
+                            PrintMatrix(matrix, size);
+                            return;
+                        }
+
+                        else if (futureElement == 'R')
+                        {
+                            Console.WriteLine("Vanko hit a rod!");
+                            hitrods++;
+                        }
+
+                        else if (futureElement == '*')
+                        {
+                            Console.WriteLine($"The wall is already destroyed at position [{vankoRow}, {vankoCol - 1}]!");
+                            matrix[vankoRow, vankoCol] = '*';
+                            vankoCol--;
+                           //countOfHoles++;
+                        }
+                    }
+
+
+
+                }
+
+
+                else if (command == "right")
+                {
+                    if (CellValid(vankoCol + 1, vankoRow, size))
+                    {
+                        char futureElement = matrix[vankoRow, vankoCol + 1];
+
+                        if (futureElement == '-')
+                        {
+                            matrix[vankoRow, vankoCol] = '*';
+                            matrix[vankoRow, vankoCol + 1] = 'V';
+                            vankoCol++;
+                            countOfHoles++;
+                        }
+
+                        else if (futureElement == 'C')
+                        {
+                            matrix[vankoRow, vankoCol] = '*';
+                            matrix[vankoRow, vankoCol + 1] = 'E';
+                            countOfHoles++;
+                            Console.WriteLine($"Vanko got electrocuted, but he managed to make {++countOfHoles} hole(s).");
+                            PrintMatrix(matrix, size);
+                            return;
+                        }
+
+                        else if (futureElement == 'R')
+                        {
+                            Console.WriteLine("Vanko hit a rod!");
+                            hitrods++;
+                        }
+
+                        else if (futureElement == '*')
+                        {
+                            Console.WriteLine($"The wall is already destroyed at position [{vankoRow}, {vankoCol + 1}]!");
+                            matrix[vankoRow, vankoCol] = '*';
+                            vankoCol++;
+                            //countOfHoles++;
+                        }
+                    }
+
+
+
+                }
+
+                else if (command == "up")
+                {
+                    if (CellValid(vankoCol, vankoRow - 1, size))
+                    {
+                        char futureElement = matrix[vankoRow - 1, vankoCol];
+
+                        if (futureElement == '-')
+                        {
+                            matrix[vankoRow, vankoCol] = '*';
+                            matrix[vankoRow - 1, vankoCol] = 'V';
+                            vankoRow--;
+                           countOfHoles++;
+                        }
+
+                        else if (futureElement == 'C')
+                        {
+                            matrix[vankoRow, vankoCol] = '*';
+                            matrix[vankoRow - 1, vankoCol] = 'E';
+                            countOfHoles++;
+                            Console.WriteLine($"Vanko got electrocuted, but he managed to make {++countOfHoles} hole(s).");
+                            PrintMatrix(matrix, size);
+                            return;
+                        }
+
+                        else if (futureElement == 'R')
+                        {
+                            Console.WriteLine("Vanko hit a rod!");
+                            hitrods++;
+                        }
+
+                        else if (futureElement == '*')
+                        {
+                            Console.WriteLine($"The wall is already destroyed at position [{vankoRow - 1}, {vankoCol}]!");
+                            matrix[vankoRow, vankoCol] = '*';
+                            vankoRow--;
+                           // countOfHoles++;
+                        }
+                    }
+
+
+
+                }
+
+                else if (command == "down")
+                {
+                    if (CellValid(vankoCol, vankoRow + 1, size))
+                    {
+                        char futureElement = matrix[vankoRow + 1, vankoCol];
+
+                        if (futureElement == '-')
+                        {
+                            matrix[vankoRow, vankoCol] = '*';
+                            matrix[vankoRow + 1, vankoCol] = 'V';
+                            vankoRow++;
+                            countOfHoles++;
+                        }
+
+                        else if (futureElement == 'C')
+                        {
+                            matrix[vankoRow, vankoCol] = '*';
+                            matrix[vankoRow + 1, vankoCol] = 'E';
+                            countOfHoles++;
+                            Console.WriteLine($"Vanko got electrocuted, but he managed to make {++countOfHoles} hole(s).");
+                            PrintMatrix(matrix, size);
+                            return;
+                        }
+
+                        else if (futureElement == 'R')
+                        {
+                            Console.WriteLine("Vanko hit a rod!");
+                            hitrods++;
+                        }
+
+                        else if (futureElement == '*')
+                        {
+                            Console.WriteLine($"The wall is already destroyed at position [{vankoRow + 1}, {vankoCol}]!");
+                            matrix[vankoRow, vankoCol] = '*';
+                            
+                            vankoRow++;
+                            //countOfHoles++;
+                        }
+                    }
+
+
+
+                }
+
+                command = Console.ReadLine();
+            }
+
+                Console.WriteLine($"Vanko managed to make {++countOfHoles} hole(s) and he hit only {hitrods} rod(s).");
+                PrintMatrix(matrix, size);
+            
+        }
+
+        private static void PrintMatrix(char[,] matrix, int size)
+        {
+            for (int row = 0; row < size; row++)
+            {
+                for (int col = 0; col < size; col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static bool CellValid(int vankoCol, int vankoRow, int size)
+        {
+            return vankoCol >= 0 && vankoCol < size && vankoRow >= 0 && vankoRow < size;
+        }
+    }
+}
