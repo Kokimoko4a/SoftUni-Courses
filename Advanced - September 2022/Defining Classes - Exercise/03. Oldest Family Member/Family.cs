10483c89d0db7e0ac3a75cc5b054759a24640260
Kokimoko4a <106096547+Kokimoko4a@users.noreply.github.com>
2022-10-05 19:06:53 +0300

    Added exercises from Defining Classes - Exercise.

---
+++ Advanced - September 2022/Defining Classes - Exercise/03. Oldest Family Member/Family.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+		private List<Person> people;
+
+		public List<Person> People
+		{
+			get { return people; }
+			set { people = value; }
+		}
+
+		 public void AddMemeber(Person member)
+		{
+			People.Add(member);	
+		}
+
+		public string GetOldestMember()
+		{
+			int age = People.Max(x=>x.Age);
+			Person person = People.FirstOrDefault(x => x.Age == age);
+			return person.Name + " " + person.Age;
+			
+		}
+
+    }
+}
