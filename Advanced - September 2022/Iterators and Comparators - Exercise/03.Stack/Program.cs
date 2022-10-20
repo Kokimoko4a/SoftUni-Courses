9544d57ef68c4b10a967439f8a7fc0e8002f2258
Kokimoko4a <106096547+Kokimoko4a@users.noreply.github.com>
2022-10-20 16:23:44 +0300

    Added some exercises from Iterators and Comparators - Exercise

---
+++ Advanced - September 2022/Iterators and Comparators - Exercise/03.Stack/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.Stack
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine().Split(new char[] { ',',' '}, StringSplitOptions.RemoveEmptyEntries).Skip(1).ToList();
+            Stack<string> stack = new Stack<string>(input);
+
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                string[] tokens = command.Split();
+
+                if (command == "Pop")
+                {
+                    try
+                    {
+                        //Console.WriteLine(stack.Pop());
+                        stack.Pop();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+
+                else if (tokens[0] == "Push")
+                {
+                    
+
+                    stack.Push(tokens.Skip(1).ToArray());
+                }
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
+
---
+++ Advanced - September 2022/Iterators and Comparators - Exercise/04.Froggy/Lake.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _04.Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        public Lake(int[] lake)
+        {
+            MyLake = lake;
+        }
+
+        public int[] MyLake { get; set; }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+           //MaLake = MyLake.OrderBy()
+
+            for (int i = 0; i < MyLake.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    yield return MyLake[i];
+                }
+            }
+
+            for (int i = MyLake.Length - 1; i >= 0; i--)
+            {
+                if (i%2!=0)
+                {
+                    yield return MyLake[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
