07df3725f129be6a2f7c64fa6ec8ae488fc51835
Kokimoko4a <106096547+Kokimoko4a@users.noreply.github.com>
2022-10-20 17:51:28 +0300

    Added all exercises to Iterators and Comparators - Exercise.

---
+++ Advanced - September 2022/Iterators and Comparators - Exercise/05.Comparing Objects/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.Comparing_Objects
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            string command =Console.ReadLine();
+
+            while (command!="END")
+            {
+                string[] tokens = command.Split();
+                Person person = new Person(tokens[0], int.Parse(tokens[1]), tokens[2]);
+                people.Add(person);
+                command = Console.ReadLine();
+
+            }
+
+            //"{count of matches} {number of not equal people} {total number of people}"
+            int indexToCompare = int.Parse(Console.ReadLine());
+            Person thePersonToCompare = people[indexToCompare-1];
+            int mathes = 0;
+            int disMathes = 0;
+
+            foreach (var item in people)
+            {
+                thePersonToCompare.CompareTo(item);
+
+                if (thePersonToCompare.CompareTo(item) == 0)
+                {
+                    mathes++;
+                }
+
+                else
+                {
+                    disMathes++;
+                }
+            }
+
+            mathes--;
+            if (mathes==0)
+            {
+                Console.WriteLine("No matches");
+                return;
+            }
+            Console.WriteLine($"{mathes} {disMathes} {people.Count}");
+        }
+    }
+}
