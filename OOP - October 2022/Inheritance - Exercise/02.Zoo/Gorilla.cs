a69a7f52e17bdd30d53eafa14f03265307d50c1c
Kokimoko4a <106096547+Kokimoko4a@users.noreply.github.com>
2022-10-26 17:24:24 +0300

    Added exercise 2 to Inheritance Exercise

---
+++ OOP - October 2022/Inheritance - Exercise/02.Zoo/Gorilla.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Zoo;
+
+namespace Zoo
+{
+    public class Gorilla : Mammal
+    {
+        public Gorilla(string name) : base(name)
+        {
+
+        }
+    }
+}
