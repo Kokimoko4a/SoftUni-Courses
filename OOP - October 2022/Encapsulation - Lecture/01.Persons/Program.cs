60e68c2c41efe1245ce0b75a176c289596b0a7d2
Kokimoko4a <106096547+Kokimoko4a@users.noreply.github.com>
2022-10-28 23:02:08 +0300

    Added 1 exercise to Encapsulation - Lecture.

---
+++ OOP - October 2022/Encapsulation - Lecture/01.Persons/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] arguments = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Person person = new Person(arguments[0], arguments[1], int.Parse(arguments[2]));
+                people.Add(person);
+            }
+
+            people.OrderBy(x => x.FirstName).ThenBy(x => x.Age).ToList().ForEach(p=> Console.WriteLine(p.ToString()));
+        }
+    }
+}
