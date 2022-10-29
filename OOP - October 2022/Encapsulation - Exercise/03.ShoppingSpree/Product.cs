09cc5d871f3725120b2685ffcf2f18d9414af171
Kokimoko4a <106096547+Kokimoko4a@users.noreply.github.com>
2022-10-29 17:42:22 +0300

    Added exercise 3 to Encapsulation - Exercise.

---
+++ OOP - October 2022/Encapsulation - Exercise/03.ShoppingSpree/Product.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.ShoppingSpree
+{
+    public class Product
+    {
+        public Product(string name, int cost)
+        {
+            Name = name;
+            Cost = cost;
+        }
+
+        private string name;
+        private int cost;
+
+        public string Name
+        {
+            get { return name; }
+
+            set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public int Cost
+        {
+            get { return cost; }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+
+                this.cost = value;
+            }
+        }
+    }
+}
