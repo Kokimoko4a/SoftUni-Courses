a23e1ca5a8c81453361cc52254faf09d79b660c8
Kokimoko4a <106096547+Kokimoko4a@users.noreply.github.com>
2022-11-02 17:03:35 +0200

    Added exercise 1 to Interfaces and Abstraction - Lecture

---
+++ Interfaces and Abstraction - Lecture/Shapes/IDrawable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    internal interface IDrawable
+    {
+        public void Draw();
+
+    }
+}
---
+++ Interfaces and Abstraction - Lecture/Shapes/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+           Circle circle = new Circle(5);
+            circle.Draw();
+
+            Rectangle rectangle = new Rectangle(3, 4);
+            rectangle.Draw();
+        }
+    }
+}
