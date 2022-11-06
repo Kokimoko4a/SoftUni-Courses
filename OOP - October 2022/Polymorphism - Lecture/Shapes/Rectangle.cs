0e9b2f0dea2bbe934380afa4d7ace940eddb1f05
Kokimoko4a <106096547+Kokimoko4a@users.noreply.github.com>
2022-11-06 10:08:45 +0200

    Added exercises from Polymorphism - Lecture

---
+++ OOP - October 2022/Polymorphism - Lecture/Shapes/Rectangle.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double height;
+        private double width;
+
+
+        public Rectangle(double height, double width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public double Height
+        {
+            get
+            {
+                return height;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException();
+                }
+
+                height = value;
+            }
+        }
+
+
+        public double Width
+        {
+            get
+            {
+                return width;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException();
+                }
+
+                width = value;
+            }
+        }
+
+        public override double CalculateArea()
+        {
+            return Width * Height;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return (2 * Width) + (2 * Height);
+        }
+
+        public override string Draw()
+        {
+            return 
+        }
+    }
+}
