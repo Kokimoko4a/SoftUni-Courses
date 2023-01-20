75c49de73c9e384d8813d9dab3652e0448e6457e
Kokimoko4a <106096547+Kokimoko4a@users.noreply.github.com>
2023-01-20 16:17:06 +0200

    Added ex.9 to Table Relations

---
+++ MS SQL - January 2023/Table Relations/09. Peaks in Rila.sql
@@ -0,0 +1,8 @@
+USE [Geography]
+
+SELECT MountainRange,PeakName,Elevation
+  FROM Mountains AS [m]
+   JOIN Peaks AS [p]
+   ON m.Id = p.MountainId 
+  WHERE m.MountainRange = 'Rila'
+ ORDER BY Elevation DESC
