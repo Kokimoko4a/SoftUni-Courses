77b47ba14a18d92e2a5be0d553229cb55bdba2f0
Kokimoko4a <106096547+Kokimoko4a@users.noreply.github.com>
2023-01-27 16:40:21 +0200

    Added exercises to Subqueries and Joins

---
+++ MS SQL - January 2023/Subqueries and Joins/06. Employees Hired After.sql
@@ -0,0 +1,12 @@
+ SELECT [FirstName]
+       ,[LastName]
+	   ,[HireDate]
+	   ,d.[Name] AS [DeptName]
+ FROM Employees AS e
+ JOIN Departments AS d
+ ON
+ e.DepartmentID = d.DepartmentID
+ WHERE e.HireDate > '1.1.1999'
+ AND
+ d.[Name] IN ('Sales','Finance') 
+ ORDER BY e.HireDate
