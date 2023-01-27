1ceed9df0a3272d49f024716f35cbf7e513fbaea
Kokimoko4a <106096547+Kokimoko4a@users.noreply.github.com>
2023-01-27 16:39:27 +0200

    Added exercises to Subqueries and Joins

---
+++ MS SQL - January 2023/Subqueries and Joins/01. Employee Address.sql
@@ -0,0 +1,12 @@
+USE [SoftUni]
+
+ SELECT TOP(5) 
+         e.[EmployeeID]
+	    ,e.[JobTitle]
+		,e.AddressID
+		,a.AddressText
+ FROM Employees AS e
+ LEFT JOIN Addresses AS a 
+ ON
+ e.AddressID = a.AddressID
+ ORDER BY a.AddressID 
---
+++ MS SQL - January 2023/Subqueries and Joins/02. Addresses with Towns.sql
@@ -0,0 +1,15 @@
+USE SoftUni
+
+ SELECT TOP(50)
+		    e.[FirstName]
+			,[e].LastName
+			,[t].[Name]
+			,[a].AddressText
+ FROM Employees AS e
+ JOIN Addresses AS a
+ ON
+ e.AddressID = a.AddressID
+ JOIN Towns AS t
+ ON
+ a.TownID = t.TownID
+ ORDER BY e.FirstName,e.LastName
