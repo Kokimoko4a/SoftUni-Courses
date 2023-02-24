3c5f309f22c2810b8627c8c767852586f9bc67a6
Kokimoko4a <106096547+Kokimoko4a@users.noreply.github.com>
2023-02-24 16:42:43 +0200

    Added exercises to Entity Framework Introduction - Exercise

---
+++ Entity Framework Core - February 2023/Entity Framework Introduction - Exercise/Entity Framework Intro/StartUp.cs
@@ -0,0 +1,76 @@
+﻿using SoftUni;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+using SoftUni.Data;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+            //Console.WriteLine(GetEmployeesWithSalaryOver50000(context));
+        }
+
+        //Problem 3
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context.Employees
+                .Select(x => new
+                {
+                    x.FirstName,
+                    x.MiddleName,
+                    x.LastName,
+                    x.JobTitle,
+                    x.Salary,
+                    x.EmployeeId
+                })
+                .OrderBy(x => x.EmployeeId).ToArray();
+
+
+            foreach (var item in employees)
+            {
+                sb.AppendLine($"{item.FirstName} {item.LastName} {item.MiddleName} {item.JobTitle} {item.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        //Problem 4
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context.Employees.Select(x => new { x.FirstName, x.Salary })
+                .Where(x => x.Salary > 50000).OrderBy(x => x.FirstName).ToArray();
+
+            foreach (var item in employees)
+            {
+                sb.AppendLine($"{item.FirstName} - {item.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        //Problem 5
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context.Employees.Select(x=> new {x.Salary,x.Department , x.FirstName, x.LastName })
+                .Where(x => x.Department.Name == "Research and Development").OrderBy(x=>x.Salary)
+                .ThenByDescending(x=>x.FirstName).ToArray();
+
+            foreach (var item in employees)
+            {
+                sb.AppendLine($"{item.FirstName} {item.LastName} from Research and Development - ${item.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
