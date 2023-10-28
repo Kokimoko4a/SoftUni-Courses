c881642a2d5812a2db0c615f02166a120171bc0e
Kokimoko4a <106096547+Kokimoko4a@users.noreply.github.com>
2023-10-28 12:29:11 +0300

    Added exercise to JS Funds folder.

---
+++ JS Front-End - October 2023/JS Syntax Fundamentals/06. Largest Number.js
@@ -0,0 +1,25 @@
+function findBiggestNumber(number1, number2, number3) {
+
+    if (number1 > number2 && number1 > number3) {
+
+        console.log(`The largest number is ${number1}.`);
+
+    }
+
+    else if (number2 > number1 && number2 > number3) {
+
+        console.log(`The largest number is ${number2}.`);
+
+    }
+
+    else if (number3 > number1 && number3 > number2) {
+
+        console.log(`The largest number is ${number3}.`);
+
+    }
+
+
+}
+
+
+
