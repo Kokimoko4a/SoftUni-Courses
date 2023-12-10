5c93db22d4c810cfadf54074949a2f779c4acc78
Kokimoko4a <106096547+Kokimoko4a@users.noreply.github.com>
2023-12-10 14:35:49 +0200

    Added new exercise to HTTP and AJAX folder.

--- JS Front-End - October 2023/HTTP and AJAX-Exercise/HTTP and AJAX-Exercise/04.Students/app.js
+++ JS Front-End - October 2023/HTTP and AJAX-Exercise/HTTP and AJAX-Exercise/04.Students/app.js
@@ -1,30 +1,57 @@
-function attachEvents() {
+async function attachEvents() {
 
   const baseURL = "http://localhost:3030/jsonstore/collections/students";
 
-  let fistName = document.getElementById('firstName');
-  let lastName = document.getElementById('lastName');
-  let facultyNumber = document.getElementById('facultyNumber');
-  let grade = document.getElementById('grade');
+  let firstName = document.getElementsByName('firstName')[0];
+  let lastName = document.getElementsByName('lastName')[0];
+  let facultyNumber = document.getElementsByName('facultyNumber')[0];
+  let grade = document.getElementsByName('grade')[0];
+  
   let buttonSubmit = document.getElementById('submit');
+  let table = document.querySelector('tbody');
+
+  let response = await fetch(baseURL);
+
+  let students = await response.json();
+
+  Object.values(students).forEach(student => {
+
+    let tr = document.createElement('tr');
+    let tdForFirstName = document.createElement('td');
+    let tdForLastName = document.createElement('td');
+    let tdForFacultyNumber = document.createElement('td');
+    let tdForGrade = document.createElement('td');
+
+    tdForFacultyNumber.textContent = student.facultyNumber;
+    tdForFirstName.textContent = student.firstName;
+    tdForGrade.textContent = student.grade;
+    tdForLastName.textContent = student.lastName;
+
+
+    table.appendChild(tr);
+    tr.appendChild(tdForFirstName);
+    tr.appendChild(tdForLastName);
+    tr.appendChild(tdForGrade);
+    tr.appendChild(tdForFacultyNumber);
+  })
 
   buttonSubmit.addEventListener('click', async () => {
 
-  let student = {
 
-    grade: grade.value,
-    facultyNumber: facultyNumber.value,
-    firstName: fistName.value,
-    lastName: lastName.value
-  };
-  
-  await fetch(baseURL, {
+ 
+    let student = {
+      grade: grade.value,
+      facultyNumber: facultyNumber.value,
+      firstName: firstName.value,
+      lastName: lastName.value,
+    };
 
-    method: 'POST',
-    body: JSON.stringify(student)
-  });
+    await fetch(baseURL, {
+      method: 'POST',
+      body: JSON.stringify(student),
+    });
 
-  let response
+   
 
   });
 
