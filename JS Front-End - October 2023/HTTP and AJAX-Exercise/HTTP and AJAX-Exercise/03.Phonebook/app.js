f4ddef9ed3b018b986a033a3f9e4ad4d6b0531dd
Kokimoko4a <106096547+Kokimoko4a@users.noreply.github.com>
2023-12-10 11:08:54 +0200

    Addedd more exercise

--- JS Front-End - October 2023/HTTP and AJAX-Exercise/HTTP and AJAX-Exercise/03.Phonebook/app.js
+++ JS Front-End - October 2023/HTTP and AJAX-Exercise/HTTP and AJAX-Exercise/03.Phonebook/app.js
@@ -1,5 +1,69 @@
 function attachEvents() {
-    // TODO:
+
+    const baseURL = 'http://localhost:3030/jsonstore/phonebook';
+    let buttonForLoading = document.getElementById('btnLoad');
+    let phoneBook = document.getElementById('phonebook');
+    let buttonCreate = document.getElementById('btnCreate');
+    let personName = document.getElementById('person');
+    let phoneNumber = document.getElementById('phone');
+
+    buttonForLoading.addEventListener('click', async () => {
+
+        let response = await fetch(baseURL);
+
+        let peopleAndPhoneNumbersJson = await response.json();
+
+        for (const obj of Object.values(peopleAndPhoneNumbersJson)) {
+
+            let li = document.createElement('li');
+            li.textContent = `${obj.person}: ${obj.phone}`;
+            let deleteButton = document.createElement('button');
+            deleteButton.textContent = "Delete";
+            li.appendChild(deleteButton);
+            phoneBook.appendChild(li);
+
+            deleteButton.addEventListener('click', deletePhoneNumber);
+
+            async function deletePhoneNumber() {
+
+                let response = await fetch(baseURL + `/${obj._id}`, {
+
+                    method: 'DELETE',
+
+                });
+
+                li.remove();
+
+
+            }
+        }
+
+    });
+
+    buttonCreate.addEventListener('click', async () => {
+
+
+        let phoneAndPersobObj = {
+
+            person: personName.value,
+            phone: phoneNumber.value
+        };
+
+        await fetch(baseURL, {
+
+            method: 'POST',
+            body: JSON.stringify(phoneAndPersobObj),
+
+        });
+
+        personName.value = "";
+        phoneNumber.value = '';
+
+        buttonForLoading.click();
+
+    })
+
+
 }
 
 attachEvents();
--- JS Front-End - October 2023/HTTP and AJAX-Exercise/HTTP and AJAX-Exercise/04.Students/app.js
+++ JS Front-End - October 2023/HTTP and AJAX-Exercise/HTTP and AJAX-Exercise/04.Students/app.js
@@ -1,5 +1,34 @@
 function attachEvents() {
-  // TODO:
+
+  const baseURL = "http://localhost:3030/jsonstore/collections/students";
+
+  let fistName = document.getElementById('firstName');
+  let lastName = document.getElementById('lastName');
+  let facultyNumber = document.getElementById('facultyNumber');
+  let grade = document.getElementById('grade');
+  let buttonSubmit = document.getElementById('submit');
+
+  buttonSubmit.addEventListener('click', async () => {
+
+  let student = {
+
+    grade: grade.value,
+    facultyNumber: facultyNumber.value,
+    firstName: fistName.value,
+    lastName: lastName.value
+  };
+  
+  await fetch(baseURL, {
+
+    method: 'POST',
+    body: JSON.stringify(student)
+  });
+
+  let response
+
+  });
+
+
 }
 
 attachEvents();
