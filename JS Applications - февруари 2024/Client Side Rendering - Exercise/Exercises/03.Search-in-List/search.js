da80cd706ad60966d65d621c37e2f087096ad206
Kokimoko4a <106096547+Kokimoko4a@users.noreply.github.com>
2024-03-13 17:19:55 +0200

    Added exercise to folder named Client Side Rendering

--- JS Applications - февруари 2024/Client Side Rendering - Exercise/Exercises/03.Search-in-List/search.js
+++ JS Applications - февруари 2024/Client Side Rendering - Exercise/Exercises/03.Search-in-List/search.js
@@ -1,3 +1,46 @@
-function search() {
-   // TODO
+import { html, render } from '/node_modules/lit-html/lit-html.js'
+import { towns } from './towns.js';
+
+document.querySelector('button').addEventListener('click', search);
+
+let townsRoot = document.getElementById('towns');
+let resultRoot = document.getElementById('result');
+let inputRef = document.getElementById('searchText');
+
+update(null)
+
+function update(match){
+
+   let res = ulTemplate(towns,match);
+   render(ulTemplate(towns,match),townsRoot);
 }
+
+function ulTemplate(towns, match){
+
+   return html `<ul>${towns.map((town,i) => createLiTemplate(town,match?.includes(town),i))}</ul>`
+}
+
+function createLiTemplate(town,isActive,id){
+
+   return html`
+   
+      <li id=${id} class=${isActive ? 'active' : ''}>${town}
+
+      </li>
+   `
+}
+
+function search(){
+
+   let searchText = inputRef.value;
+
+   let match = towns.filter(towns => towns.includes(searchText));
+   update(match);
+   renderMatchCount(match.length);
+}
+
+function renderMatchCount(count){
+
+   render(html`${count} matches found`, resultRoot)
+}
+
