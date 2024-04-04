3b2d756b3ccbff49a448099a39dc92081f433757
Kokimoko4a <106096547+Kokimoko4a@users.noreply.github.com>
2024-04-05 00:08:59 +0300

    Added implementation of SignalR. This will help for the GGMS. To be continued ... Enjoy ;)

---
+++ SignalR_Implementation/Models/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace YourNamespace
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string name, string message)
+        {
+            await Clients.All.SendAsync("broadcastMessage", name, message);
+        }
+    }
+}
